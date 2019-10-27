be4a7f94608b51074d257561e90827b488e83a34
David Hamilton <dahamilton10@yahoo.com>
2019-10-27 12:02:10 -0400

    added a function called change color, and some functions to time.js
    the time.js will house the functions for what to do for every possible hour of the day. I plan on having if statements for each one and based on the output of  moment().startOf('day').fromNow();
    I plan on setting the colors of all of the textareas

--- javascript.js
+++ javascript.js
@@ -7,18 +7,35 @@ var text5;
 var text6;
 var text7;
 var text8;
+var time;
+var date;
+var dateElement = $(".date-element");
+var hour0Element = $(".hour-0");
 
 $(".saveBtn-0").on("click", function (event) {
     event.preventDefault();
-    text0 = $(".hour-0").val();
+    text0 = hour0Element.val();
     localStorage.setItem("text0", text0);
 });
 
 function pullSavedText() {
-    if (localStorage.text0){
+    if (localStorage.text0) {
         text0 = localStorage.getItem("text0");
         $(".hour-0").val(text0);
+    };
+};
+
+function changeColor() {
+    time = moment().startOf('day').fromNow();
+    console.log(time);
+    if (time == "12 hours ago"){
+        console.log("working");
     }
 };
 
-pullSavedText()
+date = moment().format("LL");
+dateElement.text(date);
+
+
+pullSavedText();
+changeColor();
---
+++ time.js
@@ -0,0 +1,95 @@
+function itIs12AM(){
+    alert("hi");
+}
+
+function itIs1AM(){
+
+}
+
+function itIs2AM(){
+
+}
+
+function itIs3AM(){
+
+}
+
+function itIs4AM(){
+
+}
+
+function itIs5AM(){
+
+}
+
+function itIs6AM(){
+
+}
+
+function itIs7AM(){
+
+}
+
+function itIs8AM(){
+
+}
+
+function itIs9AM(){
+
+}
+
+function itIs10AM(){
+
+}
+
+function itIs11AM(){
+
+}
+
+function itIs12PM(){
+
+}
+
+function itIs1PM(){
+
+}
+
+function itIs2PM(){
+
+}
+
+function itIs3PM(){
+
+}
+
+function itIs4PM(){
+
+}
+
+function itIs5PM(){
+
+}
+
+function itIs6PM(){
+
+}
+
+function itIs7PM(){
+
+}
+
+function itIs8PM(){
+
+}
+
+function itIs9PM(){
+
+}
+
+function itIs10PM(){
+
+}
+
+function itIs11PM(){
+
+}
