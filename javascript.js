4c168707cec3262f813a322b5f9aa8f40b1da2c3
David Hamilton <dahamilton10@yahoo.com>
2019-10-27 13:14:09 -0400

    finished the if statements that call the time functions. the page now correctly pulls the users time and adds classes to the text areas properly

--- javascript.js
+++ javascript.js
@@ -36,8 +36,100 @@ function pullSavedText() {
 function changeColor() {
     time = moment().startOf('day').fromNow();
     console.log(time);
-    if (time == "12 hours ago"){
+    if (time == "0 hours ago"){
         console.log("working");
+        itIs12AM();
+    };
+    if (time == "1 hours ago"){
+        console.log("1 AM");
+        itIs1AM();
+    };
+    if (time == "2 hours ago"){
+        console.log("2 AM");
+        itIs2AM();
+    };
+    if (time == "3 hours ago"){
+        console.log("3 AM");
+        itIs3AM();
+    };
+    if (time == "4 hours ago"){
+        console.log("4 AM");
+        itIs4AM();
+    };
+    if (time == "5 hours ago"){
+        console.log("5 AM");
+        itIs5AM();
+    };
+    if (time == "6 hours ago"){
+        console.log("6 AM");
+        itIs6AM();
+    };
+    if (time == "7 hours ago"){
+        console.log("7 AM");
+        itIs7AM();
+    };
+    if (time == "8 hours ago"){
+        console.log("8 AM");
+        itIs8AM();
+    };
+    if (time == "9 hours ago"){
+        console.log("9 AM");
+        itIs9AM();
+    };
+    if (time == "10 hours ago"){
+        console.log("10 AM");
+        itIs1AM();
+    };
+    if (time == "11 hours ago"){
+        console.log("11 AM");
+        itIs11AM();
+    };
+    if (time == "12 hours ago"){
+        console.log("12 PM");
         itIs12PM();
+    };
+    if (time == "13 hours ago"){
+        console.log("1 PM");
+        itIs1PM();
+    };
+    if (time == "14 hours ago"){
+        console.log("2 PM");
+        itIs2PM();
+    };
+    if (time == "15 hours ago"){
+        console.log("3 PM");
+        itIs3PM();
+    };
+    if (time == "16 hours ago"){
+        console.log("4 PM");
+        itIs4PM();
+    };
+    if (time == "17 hours ago"){
+        console.log("5 PM");
+        itIs5PM();
+    };
+    if (time == "18 hours ago"){
+        console.log("6 PM");
+        itIs6PM();
+    };
+    if (time == "19 hours ago"){
+        console.log("7 PM");
+        itIs7PM();
+    };
+    if (time == "20 hours ago"){
+        console.log("8 PM");
+        itIs8PM();
+    };
+    if (time == "21 hours ago"){
+        console.log("9 PM");
+        itIs9PM();
+    };
+    if (time == "22 hours ago"){
+        console.log("10 PM");
+        itIs10PM();
+    };
+    if (time == "23 hours ago"){
+        console.log("11 PM");
+        itIs11PM();
     };
 };
